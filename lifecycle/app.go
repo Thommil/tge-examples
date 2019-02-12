837c3921ae6e59a31e8c913b53a2db56a24af485
thommil <thomthom.millet@gmail.com>
2019-02-13 00:29:43 +0100

    renamed

---
+++ lifecycle/app.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	log "log"
+	"time"
+
+	tge "github.com/thommil/tge"
+)
+
+type LifeCycle struct {
+	Runtime    tge.Runtime
+	FPSCounter int
+	TPSCounter int
+}
+
+func (app *LifeCycle) OnCreate(settings *tge.Settings) error {
+	log.Println("OnCreate()")
+	settings.Name = "LifeCycle"
+	settings.Fullscreen = false
+	settings.Renderer.FPS = 1
+	settings.Physics.TPS = 2
+	return nil
+}
+
+func (app *LifeCycle) OnStart(runtime tge.Runtime) error {
+	log.Println("OnStart()")
+	app.Runtime = runtime
+	return nil
+}
+
+func (app *LifeCycle) OnResize(width int, height int) {
+	log.Printf("OnResize(%d, %d)\n", width, height)
+}
+
+func (app *LifeCycle) OnResume() {
+	log.Println("OnResume()")
+}
+
+func (app *LifeCycle) OnRender(elapsedTime time.Duration) {
+	log.Printf("OnRender(%v)\n", elapsedTime)
+	app.FPSCounter++
+	if app.FPSCounter > 10 {
+		app.Runtime.Stop()
+	}
+}
+
+func (app *LifeCycle) OnTick(elapsedTime time.Duration) {
+	log.Printf("OnTick(%v)\n", elapsedTime)
+	app.TPSCounter++
+}
+
+func (app *LifeCycle) OnPause() {
+	log.Println("OnPause()")
+}
+
+func (app *LifeCycle) OnStop() {
+	log.Println("OnStop()")
+}
+
+func (app *LifeCycle) OnDispose() error {
+	log.Println("OnDispose()")
+	return nil
+}
+
+func main() {
+	tge.Run(&LifeCycle{})
+}
