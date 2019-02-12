84e0cf6a3a5386418c94d72a7c81738f62af005b
thommil <thomthom.millet@gmail.com>
2019-02-12 09:28:22 +0100

    moved build

--- env.sh
+++ env.sh
@@ -3,9 +3,11 @@
 if [ "$1" == "dev" ] ; then
     export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     export GO111MODULE=off
+    go get -u github.com/thommil/tge@develop
 else
     export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/go"
     export GO111MODULE=on
+    go get -u github.com/thommil/tge@master
 fi
 
 export PATH=$PATH:$GOPATH/bin
