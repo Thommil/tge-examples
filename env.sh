5302e0394f8afba5733a3742e62948ecaaf20dd0
thommil <thomthom.millet@gmail.com>
2019-02-12 09:43:00 +0100

    removed mod support for dev

--- env.sh
+++ env.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-
-if [ "$1" == "dev" ] ; then
-    export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    export GO111MODULE=off
-else
-    export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/go"
-    export GO111MODULE=on
-fi
-
-echo "Retrieving dependencies ..."
-go get -u github.com/thommil/tge
+    
+export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$GOPATH/bin
+export GO111MODULE=off
+
+go get -u github.com/thommil/tge
